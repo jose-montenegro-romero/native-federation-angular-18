cad9b709ab28ad8c425513aed75532802f9c7909
jose-montenegro-romero <149119278+jose-montenegro-romero@users.noreply.github.com>
2024-09-16 19:07:56 -0500

    add home project, two micro frontends for demo.

--- projects/host/src/app/app.routes.ts
+++ projects/host/src/app/app.routes.ts
@@ -4,7 +4,12 @@ import { loadRemoteModule } from '@angular-architects/native-federation';
 
 export const routes: Routes = [
   {
-    path: 'my-remote-app',
+    path: '',
+    loadComponent: () =>
+      loadRemoteModule('home', './Component').then((m) => m.AppComponent),
+  },
+  {
+    path: 'remote',
     loadComponent: () =>
       loadRemoteModule('remote', './Component').then((m) => m.AppComponent),
   },
